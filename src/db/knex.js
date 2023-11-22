0722a75be07d2202a0731e48785cf514837c242a
myu <myu_uehara@mail.toyota.co.jp>
2023-11-22 13:13:51 +0900

    1313

--- src/db/knex.js
+++ src/db/knex.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 const knex = require('knex');
 const knexConfig = require('./knexfile');
 const env = process.env.NODE_ENV;
+console.log('process,env : ', process, env);
 
 module.exports = knex(knexConfig[env]);
