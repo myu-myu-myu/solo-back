6a8246446c1a692fd8ff4362bc2f6fb447b17ba0
myu <myu_uehara@mail.toyota.co.jp>
2023-11-22 14:48:59 +0900

    1448

--- knexfile.js
+++ knexfile.js
@@ -20,18 +20,18 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      hostname: 'dpg-clehuok15k1s73acr97g-a',
-      port: '5432',
-      database: process.env.DATABASE_URL || 'solo_yczr',
-      username: 'user',
-      password: 'C0fuFGfICflGvKTdLOATkHB82WKjeuSe',
-      migrations: {
-        directory: './migrations',
-      },
-      seeds: {
-        directory: './seeds',
-      },
+    connection: process.env.DATABASE_URL,
+    // connection: {
+    //   hostname: 'dpg-clehuok15k1s73acr97g-a',
+    //   database: process.env.DATABASE_URL || 'solo_yczr',
+    //   username: 'user',
+    //   password: 'C0fuFGfICflGvKTdLOATkHB82WKjeuSe',
+    // },
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
     },
   },
 };
